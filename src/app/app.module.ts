385a038633bfa5d7ba66fd97a361b1630637cbbd
Naved Rasul <navedrasul@gmail.com>
2020-10-26 03:06:07 +0500

    Added Angular Material + Top Toolbar template + Side-Nav template
    - 'Angular Material' library
    - The Top Toolbar and the Side-Nav are in template form. The actual UI elements are yet to be placed.
    - Additionally, adjuatments have been made to the overall layout of the app to cover the viewport (the entire display area within the browser) by the 'mat-drawer' component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+// import { MatDividerModule } from '@angular/material/divider';
+
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
@@ -17,7 +26,14 @@ import { HomeComponent } from './home/home.component';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    RouterModule
+    RouterModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    // MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
